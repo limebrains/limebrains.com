3b3399016f4e462fc5f39e524438a86a28fac539
Bartosz Szymański <bartosz.szymanski@limebrains.com>
2019-07-30 11:53:27 +0200

    Divided sections to components

---
+++ src/components/sections/areas.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { Section } from '../section';
+import { colors } from '../theme/colors';
+import { SectionHeading } from '../heading';
+import { Slide, Flip, Fade } from 'react-reveal';
+import { IconRounded } from '../icon';
+import { Flex, Box } from '../flex';
+import Particles from 'react-particles-js';
+
+
+export const Areas = () => {
+    return(
+        <Section
+      id='areas'
+      background={colors.sections.whiteSection.background}
+      size={'xs'}
+    >
+      <Particles
+        width={'100%'}
+        params={{
+          particles: {
+            line_linked: {
+              enable: false
+            },
+            number: {
+              value: 5,
+            },
+            color: {
+              value: colors.primary.dark,
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                speed: 4,
+                size_min: 0.3
+              }
+            },
+            move: {
+              random: true,
+              speed: 1,
+              direction: "top",
+              out_mode: "out"
+            }
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse"
+              }
+            }
+          }
+        }}
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: 0,
+        }}
+      />
+{/* [{title: 'Progressive apps', subtitle: '', icon: ''}] */}
+
+      <Slide left fraction={1}>
+        <SectionHeading title={'Our areas of interest'}/>
+      </Slide>
+        {/* TODO 'isMobile() ? 'row' : 'column'} */}
+
+      <Flex flex={1} pt={'2rem'} align={'center'} justify={'center'} w={2 / 3} flexDirection={'column'}>
+        <Flex flex={1} justify={'center'} align={'center'} flexDirection={'column'}>
+          <Slide left fraction={1} >
+            <IconRounded icon={'FaLightbulb'}/>
+            <SectionHeading title={'Progressive apps'} subtitle={'ReactJS / GraphQL / NodeJS / Mobile'}/>
+          </Slide>
+        </Flex>
+        <Flex flex={1} justify={'center'} align={'center'} flexDirection={'column'}>
+          <Slide bottom fraction={1}>
+            <IconRounded icon={'FaKeyboard'}/>
+            <SectionHeading title={'Scalable API'} subtitle={'AWS / Google Cloud / Terraform / Docker'}/>
+          </Slide>
+        </Flex>
+        <Flex flex={1} justify={'center'} align={'center'} flexDirection={'column'}>
+          <Slide right fraction={1}>
+            <IconRounded icon={'FaRocket'}/>
+            <SectionHeading title={'Open source'} subtitle={'Pet / Sufler / pydrill / and much more'}/>
+          </Slide>
+        </Flex>
+      </Flex>
+    </Section>
+    )
+    
+}
---
+++ src/components/sections/team.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Section } from '../section';
+import { colors } from '../theme/colors';
+import { SectionHeading } from '../heading';
+import { Flip, Fade } from 'react-reveal';
+import { IconRounded } from '../icon';
+import { Flex } from '../flex';
+import { LimeBrainsLogo } from '../theme/logos';
+import { Img } from '../img';
+
+export const Team = () => {
+    return(
+        <Section
+        id='team'
+      background={colors.sections.lightSection.background}
+    >
+      <Fade fraction={1}>
+        <SectionHeading title={'Meet The Team'}/>
+      </Fade>
+
+      <Flex flex={1} pt={'2rem'} align={'center'} justify={'center'} w={1 / 2}>
+        <Flex flex={2} align={'center'} justify={'center'}>
+          <Flip right fraction={1}>
+            <Img src={LimeBrainsLogo} responsive width={200}/>
+          </Flip>
+        </Flex>
+        <Flex flex={1} flexDirection={'column'} justify={'center'} align={'flex-end'} pr={'5rem'}>
+          <Flex pb={'1rem'} pl={'2rem'}>
+            <IconRounded icon={'FaLightbulb'}/>
+            <Flex pl={'1rem'}>
+              <SectionHeading title={'Idea'}/>
+            </Flex>
+          </Flex>
+          <Flex pb={'1rem'} pl={'2rem'}>
+            <IconRounded icon={'FaKeyboard'}/>
+            <Flex pl={'1rem'}>
+              <SectionHeading title={'Code'}/>
+            </Flex>
+          </Flex>
+          <Flex pb={'1rem'} pl={'2rem'}>
+            <IconRounded icon={'FaRocket'}/>
+            <Flex pl={'1rem'}>
+              <SectionHeading title={'Success'}/>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
+</Section>
+    )
+}
