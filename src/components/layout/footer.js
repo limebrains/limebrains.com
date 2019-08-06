06aed057d531def605e17befe8d6208fcc45f0dc
Bartosz Szymański <bartosz.szymanski@limebrains.com>
2019-08-06 14:26:03 +0200

    Added hamburger menu for mobile site

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -123,7 +123,6 @@ export const Footer = () => {
 }
 
 if (typeof window !== "undefined") {
-  // eslint-disable-next-line global-require
   require("smooth-scroll")('a[href*="#"]', {
 		speed: 800,
 		speedAsDuration: true,
