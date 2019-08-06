8aebb1c965c26864fdde7a57038b03929310d5e7
Bartosz Szymański <bartosz.szymanski@limebrains.com>
2019-08-06 14:26:03 +0200

    Added hamburger menu for mobile site

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Heading } from 'rendition'
 import styled from 'styled-components'
 
@@ -10,6 +10,9 @@ import { gradients } from './../theme/colors';
 import { LimeBrainsLogo } from './../theme/logos'
 import { Button } from './../buttons';
 import { Flip } from 'react-reveal';
+import { BrowserView, MobileView } from 'react-device-detect'
+
+import './header.css'
 
 const HeaderWrapper = styled.header`
   max-height: 100px;
@@ -31,8 +34,26 @@ const NavButton = styled(Button)`
 `
 
 
-const Header = ({siteTitle}) => (
-  <HeaderWrapper>
+const Header = function ({siteTitle}) {
+
+  useEffect(() => {
+    setInterval(() => {
+      var menu = document.getElementById('menuToggle');
+      var closeIcon = document.getElementById("closeIcon");
+      if(closeIcon){
+        menu.addEventListener('click', handleMenuClick);
+        function handleMenuClick(event) {
+          if (event.target.nodeName === 'BUTTON') {
+            closeIcon.checked = false;
+          }
+        }
+    }
+    }, 1000)
+      
+  })
+
+  return (
+    <HeaderWrapper>
     <Flex
       justify
       align
@@ -60,6 +81,7 @@ const Header = ({siteTitle}) => (
         align={'flex-end'}
         alignSelf={'center'}
       >
+      <BrowserView>
         <ul>
           <li>
             <Link to={'#about'}>
@@ -105,10 +127,70 @@ const Header = ({siteTitle}) => (
             </Link>
           </li>
         </ul>
+      </BrowserView>
+
+      
+      <MobileView>
+        <nav role="navigation">
+            <div id="menuToggle">
+            <input id="closeIcon" type="checkbox" />
+            
+            <span></span>
+            <span></span>
+            <span></span>
+
+            <ul id="menu">
+                  <li>
+                    <Link to={'#about'}>
+                      <NavButton outline white text>
+                        ABOUT
+                      </NavButton>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={'#clients'}>
+                      <NavButton outline white text>
+                        CLIENTS
+                      </NavButton>
+                    </Link>
+                  </li>
+                  <li>
+                    <a href='https://github.com/limebrains/' style={{ textDecoration: 'none' }}>
+                      <NavButton outline white text>
+                        GITHUB
+                      </NavButton>
+                    </a>
+                  </li>
+                  <li>
+                    <Link to={'#blog'}>
+                      <NavButton outline white text>
+                        BLOG
+                      </NavButton>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={'#team'}>
+                      <NavButton outline white text>
+                        TEAM
+                      </NavButton>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={'#contact'}>
+                      <NavButton outline white>
+                        CONTACT
+                      </NavButton>
+                    </Link>
+                  </li>
+                </ul>
+          </div>
+        </nav>
+      </MobileView>
       </Flex>
     </Flex>
   </HeaderWrapper>
-)
+  ) 
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
