7631e245a90410e8c5f273d47d05d93ee9c168f1
Bartosz Szymański <bartosz.szymanski@limebrains.com>
2019-08-08 10:55:54 +0200

    Prettier code

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -1,112 +1,111 @@
 import React from 'react'
-import { Section } from '../section';
-import { colors } from '../theme/colors';
-import { SectionHeading } from '../heading';
-import { Flip } from 'react-reveal';
-import { IconRounded } from '../icon';
-import { Flex, Box } from '../flex';
-import Particles from 'react-particles-js';
-import { LimeBrainsLogo } from '../theme/logos';
-import { Text } from '../text';
-import { Img } from '../img';
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import 'react-circular-progressbar/dist/styles.css';
-import AnimatedProgressProvider from "./AnimatedProgressProvider";
-import { easeQuadInOut } from "d3-ease";
+import { Section } from '../section'
+import { colors } from '../theme/colors'
+import { SectionHeading } from '../heading'
+import { Flip } from 'react-reveal'
+import { IconRounded } from '../icon'
+import { Flex, Box } from '../flex'
+import Particles from 'react-particles-js'
+import { LimeBrainsLogo } from '../theme/logos'
+import { Text } from '../text'
+import { Img } from '../img'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+import AnimatedProgressProvider from './AnimatedProgressProvider'
+import { easeQuadInOut } from 'd3-ease'
 
 export const About = () => {
-    return (
-        <Section
-      id='about'
-      background={colors.sections.lightSection.background}
-    >
+  return (
+    <Section id="about" background={colors.sections.lightSection.background}>
       <Flex p={'2rem'} align={'center'} justify={'center'} flex={1} w={3 / 4}>
         <Box flex={1}>
-          
           <Flip right fraction={1}>
-            <SectionHeading title={'ABOUT US'}/>
+            <SectionHeading title={'ABOUT US'} />
           </Flip>
           <Text align={'center'}>
-            We are The Software House where business questions meet software answers.
-            <br/>
-            <br/>
-            We provide web and mobile solutions for: enterprises which want to automate.
-
+            We are The Software House where business questions meet software
+            answers.
+            <br />
+            <br />
+            We provide web and mobile solutions for: enterprises which want to
+            automate.
             <Flex flex={1} justify={'center'} align={'center'} pt={'2rem'}>
               <Flex flex={1} justify={'center'} align={'center'}>
-                <IconRounded icon={'FaLightbulb'}/>
+                <IconRounded icon={'FaLightbulb'} />
               </Flex>
               <Flex flex={1} justify={'center'} align={'center'}>
-                <IconRounded icon={'FaKeyboard'}/>
+                <IconRounded icon={'FaKeyboard'} />
               </Flex>
               <Flex flex={1} justify={'center'} align={'center'}>
-                <IconRounded icon={'FaRocket'}/>
+                <IconRounded icon={'FaRocket'} />
               </Flex>
               <Flex flex={1} justify={'center'} align={'center'}>
-                <IconRounded icon={'FaLightbulb'}/>
+                <IconRounded icon={'FaLightbulb'} />
               </Flex>
             </Flex>
           </Text>
           <Flex flex={1} pt={'2rem'} justify={'center'} align={'center'}>
-          <Flex flex={1} justify={'center'} align={'center'}>
-            <Flip right fraction={1} pr={'2rem'}>
-              {/* <Img src={LimeBrainsLogo} responsive width={200}/> */}
-              <AnimatedProgressProvider
-                valueStart={0}
-                valueEnd={40}
-                duration={5}
-                easingFunction={easeQuadInOut}
-              >
-                {value => {
-                  const roundedValue = Math.round(value);
-                  return (
-                    <CircularProgressbar
-                      value={value}
-                      text={`${roundedValue}
+            <Flex flex={1} justify={'center'} align={'center'}>
+              <Flip right fraction={1} pr={'2rem'}>
+                {/* <Img src={LimeBrainsLogo} responsive width={200}/> */}
+                <AnimatedProgressProvider
+                  valueStart={0}
+                  valueEnd={40}
+                  duration={5}
+                  easingFunction={easeQuadInOut}
+                >
+                  {value => {
+                    const roundedValue = Math.round(value)
+                    return (
+                      <CircularProgressbar
+                        value={value}
+                        text={`${roundedValue}
                       projektów`}
-                      maxValue={40}
-                      strokeWidth={5}
-                      /* This is important to include, because if you're fully managing the
+                        maxValue={40}
+                        strokeWidth={5}
+                        /* This is important to include, because if you're fully managing the
                       animation yourself, you'll want to disable the CSS animation. */
-                      styles={buildStyles({ 
-                        pathTransition: "none", 
-                        textSize: '12px',
-                        pathColor: '#4C7753',
-                        textColor: '#4C7753', })}
-                    />
-                  );
-                }}
-              </AnimatedProgressProvider>
-            </Flip>
+                        styles={buildStyles({
+                          pathTransition: 'none',
+                          textSize: '12px',
+                          pathColor: '#4C7753',
+                          textColor: '#4C7753',
+                        })}
+                      />
+                    )
+                  }}
+                </AnimatedProgressProvider>
+              </Flip>
             </Flex>
             <Flex flex={1} justify={'center'} align={'center'}>
-            <Flip right fraction={1}>
-            <AnimatedProgressProvider
-                valueStart={0}
-                valueEnd={25}
-                duration={3}
-                easingFunction={easeQuadInOut}
-              >
-                {value => {
-                  const roundedValue = Math.round(value);
-                  return (
-                    <CircularProgressbar
-                      value={value}
-                      text={`${roundedValue} \n \n\n\nklientów`}
-                      maxValue={25}
-                      strokeWidth={5}
-                      /* This is important to include, because if you're fully managing the
+              <Flip right fraction={1}>
+                <AnimatedProgressProvider
+                  valueStart={0}
+                  valueEnd={25}
+                  duration={3}
+                  easingFunction={easeQuadInOut}
+                >
+                  {value => {
+                    const roundedValue = Math.round(value)
+                    return (
+                      <CircularProgressbar
+                        value={value}
+                        text={`${roundedValue} \n \n\n\nklientów`}
+                        maxValue={25}
+                        strokeWidth={5}
+                        /* This is important to include, because if you're fully managing the
                       animation yourself, you'll want to disable the CSS animation. */
-                      styles={buildStyles({
-                        pathTransition: "none", 
-                        textSize: '12px',
-                        pathColor: `rgba(77, 118,	84)`,
-                        textColor: '#4C7753', })}
-                    />
-                  );
-                }}
-              </AnimatedProgressProvider>
-            </Flip>
+                        styles={buildStyles({
+                          pathTransition: 'none',
+                          textSize: '12px',
+                          pathColor: `rgba(77, 118,	84)`,
+                          textColor: '#4C7753',
+                        })}
+                      />
+                    )
+                  }}
+                </AnimatedProgressProvider>
+              </Flip>
             </Flex>
           </Flex>
         </Box>
@@ -119,9 +118,9 @@ export const About = () => {
             line_linked: {
               shadow: {
                 enable: false,
-                color: "#ffffff",
-                blur: 5
-              }
+                color: '#ffffff',
+                blur: 5,
+              },
             },
             color: {
               value: colors.primary.dark,
@@ -137,10 +136,10 @@ export const About = () => {
             events: {
               onhover: {
                 enable: true,
-                mode: "repulse"
-              }
-            }
-          }
+                mode: 'repulse',
+              },
+            },
+          },
         }}
         style={{
           width: '100%',
@@ -151,7 +150,6 @@ export const About = () => {
           zIndex: 0,
         }}
       />
-
     </Section>
-    )
+  )
 }
