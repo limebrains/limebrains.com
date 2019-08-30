36c91c2adfe1f232e274ad9a71e83f47663c3f3d
Dem07 <dariusz.bakowski@limebrains.com>
2019-08-30 14:29:40 +0200

    pseudo-working grid list of blog posts, still need some work

--- src/components/flex.js
+++ src/components/flex.js
@@ -3,12 +3,22 @@ import styled, { css } from 'styled-components'
 
 export const Flex = styled(FlexDefault)`
   z-index: 1;
+
   ${props => props.alignself ? css`align-self: ${props.alignself}` : ''}
   ${props => props.pt ? css`padding-top: ${props.pt}` : ''}
   ${props => props.pb ? css`padding-bottom: ${props.pb}` : ''}
   ${props => props.pl ? css`padding-left: ${props.pl}` : ''}
   ${props => props.pr ? css`padding-right: ${props.pr}` : ''}
   ${props => props.p ? css`padding: ${props.pr}` : ''}
+`
+
+export const FlexBlog = styled(FlexDefault)`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 10px;
+  flex-wrap: wrap;
+
+
 `
 
 export const Box = styled(BoxDefault)`
