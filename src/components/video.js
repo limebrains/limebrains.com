160ab650aea547d01bf2dad0b9c701a293133661
Dem07 <dariusz.bakowski@limebrains.com>
2019-09-06 12:43:47 +0200

    video header done

--- src/components/video.js
+++ src/components/video.js
@@ -1,33 +1,87 @@
 import React from 'react';
 import styled from 'styled-components'
 
-
-const VideoWrapper = styled.div`
+const TextWrapper = styled.div`
+  position:absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
   width: 100%;
   height: 100%;
-  max-height: 1000px;
-  line-height: 0;
-  background: #124DD9;
-  video{
-    position: relative;
-    top:0;
-    bottom:0;
-    left:0;
-    right:0;
-    width: 100% !important;
-    height: auto !important;
-  }
 `
 
+const VideoWrapper = styled.div`
+  position: relative;
+  top: 0;
+  width: 100%;
+  height: calc(100vh - 100px);
+  line-height: 0;
+
+  video{
+    position: absolute;
+    left: 50%; /* % of surrounding element */
+    top: 50%;
+    transform: translate(-50%, -50%); /* % of current element */
+    width: 100%;
+    height: 100%;
+    transition: opacity 0.5s;
+    object-fit: cover;
+  }
+  h1{
+    color: #fff;
+    font-style: italic;
+    font-size: 1.5vw;
+    font-family: BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,sans-serif;
+    font-weight: 700;
+    text-shadow: #fff 0px 1px 1px;
+    user-select: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    transition: transform .6s cubic-bezier(.175,.885,.32,1.275),opacity .6s;
+    }
+
+em {
+  font-family: 'EB Garamond', serif;
+  font-size: 10vw;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+  display: block;
+  font-style:normal;
+  padding-top: 0.1em;
+  text-shadow: 0.07em 0.07em 0 rgba(0, 0, 0, 0.1);
+  
+  &::before, &::after {
+  content: "§";
+  display: inline-block;
+  -webkit-transform: rotate(90deg);
+  -moz-transform: rotate(90deg);
+  -o-transform: rotate(90deg);
+  -ms-transform: rotate(90deg);
+  transform: rotate(90deg);
+    opacity: 0.2;
+  margin: 0 0.6em;
+  font-size: 0.5em;
+}
+
+`
 const HeaderVideo = ( ) => {
   return (
+
     <VideoWrapper>
-      <video playsInline muted preload="auto" poster="https://res.cloudinary.com/studio-up/image/fetch/f_auto/https://d22ivmygv0pwjj.cloudfront.net/v2/homeHero.jpg" autoplay="autoplay" data-loaded="true"  >
+      <TextWrapper>
+        <h1><em>Thumbs up</em></h1>
+      </TextWrapper>
+
+      <video playsInline muted preload="auto" poster="https://res.cloudinary.com/studio-up/image/fetch/f_auto/https://d22ivmygv0pwjj.cloudfront.net/v2/homeHero.jpg" autoPlay="autoplay" >
           <source data-src="https://d22ivmygv0pwjj.cloudfront.net/v2/homeHero.webm" src="https://d22ivmygv0pwjj.cloudfront.net/v2/homeHero.webm" type="video/webm" />
           <source data-src="https://d22ivmygv0pwjj.cloudfront.net/v2/homeHero.mp4" src="https://d22ivmygv0pwjj.cloudfront.net/v2/homeHero.mp4" type="video/mp4" />
           <source data-src="https://d22ivmygv0pwjj.cloudfront.net/v2/homeHero.ogg" src="https://d22ivmygv0pwjj.cloudfront.net/v2/homeHero.ogg" type="video/mp4" />
           Your browser does not support the video tag.
       </video>
+
     </VideoWrapper>
   )
 }
