4718a99b79edd4bc33178f5711d4746827e858e9
Dem07 <dariusz.bakowski@limebrains.com>
2019-09-10 11:29:14 +0200

    changed font and new section

--- src/components/theme/theme.js
+++ src/components/theme/theme.js
@@ -10,6 +10,7 @@ export const theme = {
 
 injectGlobal`
   @import url('https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i');
+  
   .hvcNOh {
     font-family: 'Montserrat', sans-serif;
     font-weight: 400;
