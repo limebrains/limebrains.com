6793eff3dff2e2f3ad0ad8adb382cd6d3e117ac7
Dem07 <dariusz.bakowski@limebrains.com>
2019-09-12 12:52:44 +0200

    bars ready without scripting
    Former-commit-id: 83d48a620d1958bc35fed5c2669798b269cdaa70

--- src/components/showhidebar.js
+++ src/components/showhidebar.js
@@ -1,15 +1,59 @@
 import React from 'react';
 import styled from 'styled-components'
+import { FaChevronDown } from 'react-icons/fa'
 
 const Container = styled.div`
-  display: flex
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
+
 `
 
-const Bar = () => {
+const Sector = styled.div`
+
+  height: 50px;
+  margin-left: 5%;
+  margin-right: 5%;
+
+  p{
+    float: left;
+  }
+`
+
+const Button = styled.div`
+  display: flex;
+  float:  right;
+  width:  50px;
+  height: 100%;
+  justify-content: flex-end;
+  align-items: center;
+  
+`
+
+const Divider = styled.div`
+  height: 1px;
+  background: #D2CBCB;
+  margin-left: 5%;
+  margin-right: 5%;
+  margin-top: 2em;
+  margin-bottom: 2em;
+`
+
+const Bar = ({text}) => {
   return (
     <Container>
+      <Sector>
+        <p>{text}</p>
+        <Button>
+          <FaChevronDown size={24} />
+        </Button>
 
+      </Sector>
+      <Divider />
     </Container>
+
   )
 }
 
