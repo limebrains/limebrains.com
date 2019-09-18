dac6134adba072154886460e5ca31166639cd579
Dem07 <dariusz.bakowski@limebrains.com>
2019-09-18 10:36:21 +0200

    added key to each blog list element

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,13 +6,13 @@ import Layout from '../components/layout/index'
 import SEO from '../components/seo'
 import { Section } from '../components/section';
 import { colors } from '../components/theme/colors';
-import { SectionHeading } from '../components/heading';
+// import { SectionHeading } from '../components/heading';
 import { Card } from '../components/card';
 import { Link } from '../components/link';
 import { Img } from '../components/img';
 import { FlexBlog, Box } from '../components/flex';
-import { Reveal, Flip } from 'react-reveal';
-import { LimeBrainsLogo } from '../components/theme/logos';
+// import { Reveal, Flip } from 'react-reveal';
+// import { LimeBrainsLogo } from '../components/theme/logos';
 
 const Blog = ({ data }) => (
   <Layout>
@@ -21,18 +21,18 @@ const Blog = ({ data }) => (
     <Section
       background={colors.sections.greenSection.background}
     >
-      <Flip left wait={2000}>
+      {/* <Flip left wait={2000}>
         <Img src={LimeBrainsLogo} responsive width={150} />
       </Flip>
       <Reveal right fraction={1}>
         <SectionHeading title={'Blog'} inverse />
-      </Reveal>
+      </Reveal> */}
     </Section>
 
     <Box m={3}>
       <FlexBlog>
         {data.allMarkdownRemark.edges.map((post) => (
-          <Box >
+          <Box key={post.node.frontmatter.title}>
             <Link to={post.node.fields.slug}>
               <Card title={_.get(post, 'node.frontmatter.title')}>
                 <Img src={_.get(post, 'node.frontmatter.image')} />
