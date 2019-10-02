1cc38ff2933b170cc51e33c7b48c23b8ef4b2433
Wojciech Nowak <mail@pythonic.ninja>
2019-10-02 02:00:52 +0200

    fix typing effect on ios
    Former-commit-id: d2f11be723b90faa3dff25d9869071629fee5408

--- src/components/typer.js
+++ src/components/typer.js
@@ -3,22 +3,24 @@ import styled from 'styled-components';
 
 const HeaderText = styled.div`
   color: white;
-  
-  h1 {
-    margin-bottom: 4rem;
-  }
 `;
 
 
 class Typer extends React.Component {
-  state = {
-    title: '',
-    subtitle: '',
-    slogan: '',
-    isDeleting: false,
-    typingSpeed: 70,
-    i: 0
-  };
+  constructor(props) {
+    super(props);
+
+    const {data} = props;
+
+    this.state = {
+      title: data.title,
+      subtitle: data.subtitle,
+      slogan: '',
+      isDeleting: false,
+      typingSpeed: 100,
+      i: 0
+    };
+  }
 
   componentDidMount() {
     this.loop();
@@ -29,27 +31,16 @@ class Typer extends React.Component {
   }
 
   loop = () => {
-    const {slogan, title, subtitle, isDeleting, typingSpeed, i} = this.state;
+    const {slogan, isDeleting, typingSpeed, i} = this.state;
     const {data} = this.props;
     const j = i % data.slogan.length;
-    this.setState({
-      title: data.title.substring(0, title.length + 1),
-      typingSpeed: 70
-    })
-    if (data.title === title) {
-      this.setState({
-        subtitle: data.subtitle.substring(0, subtitle.length + 1),
-        typingSpeed: 50
-      })
-    }
 
-    if (data.subtitle === subtitle) {
-      this.setState({
-        slogan: isDeleting ?
-          data.slogan[j].substring(0, slogan.length - 1) :
-          data.slogan[j].substring(0, slogan.length + 1),
-      })
-    }
+    this.setState({
+      slogan: isDeleting ?
+        data.slogan[j].substring(0, slogan.length - 1) :
+        data.slogan[j].substring(0, slogan.length + 1),
+    });
+
     if (!isDeleting && slogan === data.slogan[j]) {
       setTimeout(() => this.setState({isDeleting: true}), 1000)
     } else if (isDeleting && slogan === '') {
@@ -60,16 +51,17 @@ class Typer extends React.Component {
   };
 
   render() {
+    const {title, subtitle, slogan} = this.state;
     return (
-      <HeaderText>
+      <HeaderText key={slogan}>
         <h1>
-          {this.state.title}
+          {title}
         </h1>
         <h3>
-          {this.state.subtitle}
+          {subtitle}
         </h3>
-        <h3>
-          {this.state.slogan}
+        <h3 key={slogan}>
+          {slogan}
         </h3>
       </HeaderText>
     )
