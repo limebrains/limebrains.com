65f37898d1f1259b14001eebcdd8ad450a599bfc
Wojciech Nowak <mail@pythonic.ninja>
2019-10-03 13:18:17 +0200

    fonts

--- src/components/theme/theme.js
+++ src/components/theme/theme.js
@@ -9,11 +9,10 @@ export const theme = {
 }
 
 injectGlobal`
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i');
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:100,300,500,900&display=swap');
 
   body {
     margin: 0;
     padding: 0;
-
   }
-`
+`;
