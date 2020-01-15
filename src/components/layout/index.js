a97398073d179bd2b312e7ea4b3a1f710daa2319
Mateusz Dargacz <mateuszdargacz@gmail.com>
2020-01-15 19:54:58 +0100

    remove redundant dependencies

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -4,14 +4,12 @@ import { Provider } from 'rendition'
 import styled from 'styled-components'
 import { StaticQuery, graphql } from 'gatsby'
 import { LocationProvider } from '@reach/router'
-import { Context as ResponsiveContext } from 'react-responsive'
 
 import Header from './header'
 import { Footer } from './footer'
 import HeaderVideo from '../video'
 import HeaderBlog from './header-blog'
 import { theme } from './../theme/theme'
-import { isMobile, isPhonePortrait, isDesktop } from '../responsive'
 
 const ContentWrapper = styled.div`
   min-height: 75vh;
@@ -42,31 +40,10 @@ const renderVideo = () => {
 class Layout extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {
-      currentWidth: typeof window !== 'undefined' ? window.innerWidth : 1200
-    }
-  }
-
-  updateDimensions = () => {
-    const currentWidth = typeof window !== 'undefined' ? window.innerWidth : this.state.currentWidth;
-    this.setState({
-      currentWidth
-    });
-    return currentWidth
-  };
-
-  componentDidMount() {
-    window.addEventListener('resize', this.updateDimensions);
-    setTimeout(this.updateDimensions, 0)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateDimensions)
   }
 
   render() {
     const { children } = this.props;
-    const { currentWidth } = this.state;
 
     return (
       <StaticQuery
@@ -82,21 +59,10 @@ class Layout extends React.PureComponent {
         render={data => (
           <>
             <Provider theme={theme}>
-              <ResponsiveContext.Provider
-                value={{
-                  deviceWidth: this.updateDimensions,
-                  width: currentWidth,
-                  isMobile: isMobile(currentWidth),
-                  isPhonePortrait: isPhonePortrait(currentWidth),
-                  isDesktop: isDesktop(currentWidth),
-                }}
-              >
                 {renderVideo()}
                 {renderHeader(data.site.siteMetadata.title)}
-
                 <ContentWrapper>{children}</ContentWrapper>
                 <Footer/>
-              </ResponsiveContext.Provider>
             </Provider>
           </>
         )}
