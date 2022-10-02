8a89933c5b202e3f8b1370048d344b66aa018954
Mateusz Dargacz <mateuszdargacz@gmail.com>
2022-10-02 17:41:06 +0200

    removed redundant imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,9 @@ import React from 'react'
 import SEO from '../components/seo'
 import Layout from '../components/layout/index'
 import { FAQ } from '../components/sections/faq'
-import { Team } from '../components/sections/team'
 import { Areas } from '../components/sections/areas'
 import { About } from '../components/sections/about'
 import { Expertise } from '../components/sections/expertise'
-import { Reviews } from '../components/sections/reviews'
 import { JobSection } from '../components/sections/benefits'
 import { Fade } from 'react-reveal';
 
